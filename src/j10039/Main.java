699586841a97edf3d24809d9f0e019c2c7f3b239
briiiqtt <briiiqtt@gmail.com>
2022-03-30 23:13:01 +0900

    10039

---
+++ src/j10039/Main.java
@@ -0,0 +1,30 @@
+package j10039;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int sum = 0;
+		for(int i = 0; i < 5; i++) {
+			int score = Integer.parseInt(br.readLine());
+			if(score<40)
+				sum += 40;
+			else
+				sum += score;
+		}
+		
+		bw.write(sum/5+"\n");
+		bw.flush();
+		bw.close();
+	}
+	
+}
